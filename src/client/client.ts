01d97b3239e4217ecc6af0687c5c96c9eb162cc3
MCTheOnly <martin.chorzewski@gmail.com>
2021-11-19 23:57:19 +0100

    basic setup

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,60 +1,24 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+'use strict'
+
+// import _ from 'lodash';
+import './style.scss';
+
+import * as THREE from 'three';
 import * as dat from 'dat.gui';
+import { DoubleSide, WebGLRenderer } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 //SCENE
 const scene = new THREE.Scene()
+const canvas = document.querySelector('canvas.webgl');
 
-//CAMERA
-const camera = new THREE.PerspectiveCamera(
-	75,
-	window.innerWidth / window.innerHeight,
-	0.1,
-	1000
-)
-camera.position.z = 2
+//GUI
+const gui = new dat.GUI();
 
-//RENDERER
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+const GUIlights = gui.addFolder('Lights');
+const GUIlight1 = GUIlights.addFolder('Light1');
+const GUIlight2 = GUIlights.addFolder('Light2');
 
-new OrbitControls(camera, renderer.domElement)
-
-//OBJECT
-const geometry = new THREE.BoxGeometry()
-
-//MATERIAL
-const material = new THREE.MeshBasicMaterial({
-	color: 0x00ff00,
-	wireframe: true,
-})
-
-//MESH
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
-
-
-window.addEventListener('resize', onWindowResize, false)
-function onWindowResize() {
-	camera.aspect = window.innerWidth / window.innerHeight
-	camera.updateProjectionMatrix()
-	renderer.setSize(window.innerWidth, window.innerHeight)
-	render()
-}
-
-//LOOP
-function animate() {
-	requestAnimationFrame(animate)
-
-	cube.rotation.x += 0.01
-	cube.rotation.y += 0.01
-
-	render()
-}
-
-function render() {
-	renderer.render(scene, camera)
-}
-
-animate()
+const GUIobjects = gui.addFolder('Objects');
+const GUIearth = GUIobjects.addFolder('Earth');
+const GUImoon = GUIobjects.addFolder('Moon');
